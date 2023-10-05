a00485554b2f25877725a31abb1cc9ca9af892c4
NikM2000 <nikolamilanovic40@gmail.com>
2023-10-04 22:34:22 -0400

    Portfolio Website, Home and Work Experience

---
+++ script.js
@@ -0,0 +1,42 @@
+function scrollToElement(elementSelector, instance = 0){
+    //Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    //Check if there are elements matching the selector and if the requested instance exists.
+    if(elements.length > instance){
+        //Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({behavior: 'smooth'});
+    }
+}
+
+function openInNewTab(url){
+    var win = window.open(url, 'blank');
+    win.focus();
+}
+
+function contactMe() {
+    window.location.href = "mailto:nikolamilanovic40@gmail.com";
+}
+
+var buttonElement = document.getElementById("contactButton");
+var linkedInButton = document.getElementById("linkedInButton");
+var gitHubButton = document.getElementById("gitHubButton");
+var contactMeButton = document.getElementById("contactMeButton");
+var buttonElement2 = document.getElementById("contactButton2");
+
+buttonElement2.addEventListener("click", () =>{
+    scrollToElement('.header');
+})
+
+buttonElement.addEventListener("click", () => {
+    scrollToElement('.header', 1);
+});
+
+linkedInButton.addEventListener("click", () =>{
+    openInNewTab("https://www.linkedin.com/in/milanovic-n/");
+});
+
+gitHubButton.addEventListener("click", () =>{
+    openInNewTab("https://github.com/NikM2000");
+});
+
+contactMeButton.addEventListener("click", contactMe);
